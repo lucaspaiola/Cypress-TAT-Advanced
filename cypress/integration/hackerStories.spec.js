a1af7ce4d70629c2c02e4687fb0d54ae58c61971
lucaspaiola <lucas.v.paiola@gmail.com>
2024-07-17 11:00:26 -0300

    Add network and server errors tests

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -67,15 +67,6 @@ describe('Hacker Stories', () => {
 
       it('orders by points', () => {})
     })
-
-    // Hrm, how would I simulate such errors?
-    // Since I still don't know, the tests are being skipped.
-    // TODO: Find a way to test them out.
-    context.skip('Errors', () => {
-      it('shows "Something went wrong ..." in case of a server error', () => {})
-
-      it('shows "Something went wrong ..." in case of a network error', () => {})
-    })
   })
 
   context('Search', () => {
@@ -186,5 +177,39 @@ describe('Hacker Stories', () => {
           .should('have.length', 5)
       })
     })
+  })
+})
+
+context('Errors', () => {
+  const errorMessage = 'Something went wrong ...'
+
+  it('shows "Something went wrong ..." in case of a server error', () => {
+    cy.intercept(
+      'GET',
+      '**/search**',
+      { statusCode: 500 }
+    ).as('getServerFailure')
+
+    cy.visit('/')
+
+    cy.wait('@getServerFailure')
+
+    cy.get(`p:contains(${errorMessage})`)
+      .should('be.visible')
+  })
+
+  it('shows "Something went wrong ..." in case of a network error', () => {
+    cy.intercept(
+      'GET',
+      '**/search**',
+      { forceNetworkError: true }
+    ).as('getNetworkFailure')
+
+    cy.visit('/')
+
+    cy.wait('@getNetworkFailure')
+
+    cy.get(`p:contains(${errorMessage})`)
+      .should('be.visible')
   })
 })
