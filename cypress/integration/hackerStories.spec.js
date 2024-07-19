c29c6ee90cb44526734b7b3d46f270d6174d00ae
lucaspaiola <lucas.v.paiola@gmail.com>
2024-07-19 15:42:58 -0300

    Add new fixture to mock tests

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -76,24 +76,24 @@ describe('Hacker Stories', () => {
   })
 
   context('Mocking the API', () => {
-    beforeEach(() => {
-      cy.intercept(
-        'GET',
-        `**/search?query=${initialTerm}&page=0`,
-        { fixture: 'stories'}
-      ).as('getStories')
-      
-      cy.visit('/')
-      cy.wait('@getStories') // wait the request conclude to continues
-    })
-  
-    it('shows the footer', () => {
-      cy.get('footer')
-        .should('be.visible')
-        .and('contain', 'Icons made by Freepik from www.flaticon.com')
-    })
-  
-    context('List of stories', () => {
+    context('Footer and list of stories', () => {
+      beforeEach(() => {
+        cy.intercept(
+          'GET',
+          `**/search?query=${initialTerm}&page=0`,
+          { fixture: 'stories'}
+        ).as('getStories')
+        
+        cy.visit('/')
+        cy.wait('@getStories') // wait the request conclude to continues
+      })
+    
+      it('shows the footer', () => {
+        cy.get('footer')
+          .should('be.visible')
+          .and('contain', 'Icons made by Freepik from www.flaticon.com')
+      })
+
       // Since the API is external,
       // I can't control what it will provide to the frontend,
       // and so, how can I assert on the data?
@@ -101,14 +101,14 @@ describe('Hacker Stories', () => {
       // TODO: Find a way to test it out.
       it.skip('shows the right data for all rendered stories', () => {})
   
-      it.only('shows one less story after dimissing the first one', () => {
+      it('shows one less story after dimissing the first one', () => {
         cy.get('.button-small')
           .first()
           .click()
   
         cy.get('.item').should('have.length', 1) // since we have 2 stories at the stories.json
       })
-  
+
       // Since the API is external,
       // I can't control what it will provide to the frontend,
       // and so, how can I test ordering?
@@ -116,34 +116,31 @@ describe('Hacker Stories', () => {
       // TODO: Find a way to test them out.
       context.skip('Order by', () => {
         it('orders by title', () => {})
-  
+
         it('orders by author', () => {})
-  
+
         it('orders by comments', () => {})
-  
+
         it('orders by points', () => {})
       })
     })
   
     context('Search', () => {
       beforeEach(() => {
-        cy.intercept({
-          method: 'GET',
-          pathname: '**/search',
-          query: {
-            query: newTerm,
-            page: '0'
-          }
-        }).as('getNewTermStories')
-  
-        cy.intercept({
-          method: 'GET',
-          pathname: '**/search',
-          query: {
-            query: initialTerm,
-            page: '0'
-          }
-        }).as('getInitialTermStories')
+        cy.intercept(
+          'GET',
+          `**/search?query=${initialTerm}&page=0`,
+          { fixture: 'empty' }
+        ).as('getEmptyList')
+
+        cy.intercept(
+          'GET',
+          `**/search?query=${newTerm}&page=0`,
+          { fixture: 'stories' }
+        ).as('getNewTermStories')
+
+        cy.visit('/')
+        cy.wait('@getEmptyList')
   
         cy.get('#search')
           .clear()
@@ -155,10 +152,7 @@ describe('Hacker Stories', () => {
   
         cy.wait('@getNewTermStories')
   
-        cy.get('.item').should('have.length', 20)
-        cy.get('.item')
-          .first()
-          .should('contain', newTerm)
+        cy.get('.item').should('have.length', 2)
         cy.get(`button:contains(${initialTerm})`)
           .should('be.visible')
       })
@@ -171,10 +165,7 @@ describe('Hacker Stories', () => {
   
         cy.wait('@getNewTermStories')
   
-        cy.get('.item').should('have.length', 20)
-        cy.get('.item')
-          .first()
-          .should('contain', newTerm)
+        cy.get('.item').should('have.length', 2)
         cy.get(`button:contains(${initialTerm})`)
           .should('be.visible')
       })
@@ -187,7 +178,7 @@ describe('Hacker Stories', () => {
   
         cy.wait('@getNewTermStories')
   
-        cy.get('.item').should('have.length', 20)
+        cy.get('.item').should('have.length', 2)
       })
   
       context('Last searches', () => {
@@ -197,7 +188,8 @@ describe('Hacker Stories', () => {
   
           cy.intercept(
             'GET',
-            '**/search**'
+            '**/search**',
+            { fixture: 'empty' }
           ).as('getRandomStories')
   
           Cypress._.times(6, () => {
